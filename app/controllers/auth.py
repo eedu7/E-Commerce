d51dae1347294e052fc8c848d80355c2d61206f8
eedu7 <mueedahmad067@gmail.com>
2024-06-30 09:06:17 +0500

    MODIFIED: Modify the AuthControoler

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -2,11 +2,11 @@ from pydantic import EmailStr
 
 from app.models import User
 from app.repositories import UserRepository
-from app.schemas.extras import Token
+from app.schemas.extras.token import Token
 from core.controller import BaseController
 from core.database import Propagation, Transactional
 from core.exceptions import BadRequestException, UnauthorizedException
-from core.security import JWTHandler
+from core.security import JWTHandler, PasswordHandler
 
 
 class AuthController(BaseController[User]):
@@ -15,8 +15,7 @@ class AuthController(BaseController[User]):
         self.user_repository = user_repository
 
     @Transactional(propagation=Propagation.REQUIRED)
-    async def register(self, email: EmailStr,
-                       username: str) -> User:
+    async def register(self, email: EmailStr, password: str, username: str) -> User:
         # Check if user exists with email
         user = await self.user_repository.get_by_email(email)
 
@@ -29,17 +28,23 @@ class AuthController(BaseController[User]):
         if user:
             raise BadRequestException("User already exists with this username")
 
+        password = PasswordHandler.hash(password)
+
         return await self.user_repository.create(
             {
                 "email": email,
+                "password": password,
                 "username": username,
             }
         )
 
-    async def login(self, email: EmailStr) -> Token:
+    async def login(self, email: EmailStr, password: str) -> Token:
         user = await self.user_repository.get_by_email(email)
 
         if not user:
+            raise BadRequestException("Invalid credentials")
+
+        if not PasswordHandler.verify(user.password, password):
             raise BadRequestException("Invalid credentials")
 
         return Token(
@@ -47,15 +52,13 @@ class AuthController(BaseController[User]):
             refresh_token=JWTHandler.encode(payload={"sub": "refresh_token"}),
         )
 
-    async def refresh_token(self, access_token: str,
-                            refresh_token: str) -> Token:
+    async def refresh_token(self, access_token: str, refresh_token: str) -> Token:
         token = JWTHandler.decode(access_token)
         refresh_token = JWTHandler.decode(refresh_token)
         if refresh_token.get("sub") != "refresh_token":
             raise UnauthorizedException("Invalid refresh token")
 
         return Token(
-            access_token=JWTHandler.encode(
-                payload={"user_id": token.get("user_id")}),
+            access_token=JWTHandler.encode(payload={"user_id": token.get("user_id")}),
             refresh_token=JWTHandler.encode(payload={"sub": "refresh_token"}),
         )
